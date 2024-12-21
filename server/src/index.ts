9e76ccb11f8d770438835c5641bd5c3f1edfbfc9
Noel Lopez <nlopez.publi@gmail.com>
2024-12-21 19:34:52 +0100

    feature(server): health check endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,12 @@ import express from "express";
 
 const app = express();
 
-// TODO: health check
+app.get("/health", (_req, res) => {
+  res.status(200).send({ status: "ok" });
+});
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
